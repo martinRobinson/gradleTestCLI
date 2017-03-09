b43daf5eb9d456ba28cfd7260e1a01c7354503e1
Martin Robinson <martin.robinson@lyit.ie>
2017-03-09 14:45:50 +0000

    Added extra method to class App.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,10 @@ public class App {
         return "Hello world.";
     }
 
+    public String getSecondGreeting() {
+        return "Hello world.";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
